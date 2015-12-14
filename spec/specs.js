c1ed3f631f854d73e11f7c7d576c1617cdeebe2d
Kelly Becker <kellywerks@gmail.com>
2015-12-14 10:53:28 -0800

    add functionality for years divisible by 100

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,13 @@
 describe('leapYear', function() {
   it("is false for a year that is not divisible by 4", function() {
-    expect(leapYear(1999)).to.equal(false);
+    expect(leapYear(1993)).to.equal(false);
+  });
+
+  it("is true for years divisible by 4", function() {
+    expect(leapYear(2004)).to.equal(true);
+  });
+
+  it("is false for years divisible by 100", function() {
+    expect(leapYear(1900)).to.equal(false);
   });
 });
