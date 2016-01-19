b9bf3d46267b07dc7f9b066cae473abad37f6161
Haley Bristol and James Turello <jturello@yahoo.com>
2016-01-19 09:05:41 -0800

    add tests for non-valid numbers

--- spec/specs.js
+++ spec/specs.js
@@ -14,4 +14,17 @@ describe('leapYear', function() {
  it("is true for years divisible by 400", function() {
     expect(leapYear(2000)).to.equal(true);
   });
+
+  it("is false for years that are words, not numbers", function() {
+     expect(leapYear("this")).to.equal(false);
+   });
+
+   it("is false for negative numbers entered for year", function() {
+      expect(leapYear(-1)).to.equal(false);
+    });
+
+    it("is false for empty strings for year", function() {
+       expect(leapYear(parseInt(""))).to.equal(false);
+     });
+
 });
